6848fcad6ace48d4b73a788659d89cdd46b5372a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-26 18:25:36 +0800

    Updated dependencies.

---
+++ .install.sh
@@ -0,0 +1,19 @@
+yarn add \
+babel-runtime \
+cemento \
+harden \
+protype \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+cemento \
+harden \
+protype \
+--save
