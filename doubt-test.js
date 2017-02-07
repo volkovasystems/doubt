e8a515d150d589f0cf73e4b44fae9d0f933dca0e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-07 18:52:19 +0800

    Update test.

--- doubt-test.js
+++ doubt-test.js
@@ -3,8 +3,11 @@
 const util = require( "util" );
 const doubt = require( "./doubt.js" );
 
-console.log( util.inspect( doubt( [ ] ), { "showHidden": true } ) );
+//console.log( util.inspect( doubt( [ ] ), { "showHidden": true } ) );
 
-console.log( doubt( [ ], ARRAY ) );
+//console.log( doubt( [ ], ARRAY ) );
 
-console.log( doubt( { } ) );
+//console.log( doubt( { } ) );
+
+//console.log( doubt( ) );
+console.log( doubt( [ 1, 2, 3 ], ARRAY ) );
