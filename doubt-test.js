755a00644953f4f1e4bb838cea84a343e33eb919
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-06 16:57:18 +0800

    Updated test.

--- doubt-test.js
+++ doubt-test.js
@@ -1,10 +1,14 @@
-const util = require( "util" );
-
 const assert = require( "assert" );
 const doubt = require( "./doubt.js" );
 
-assert.equal( doubt( Infinity, AS_ARRAY ), false, "should be false" );
+assert.equal( doubt( [ ], ARRAY ), true, "should be true" );
+assert.equal( doubt( [ ], AS_ARRAY ), true, "should be true" );
+assert.equal( doubt( arguments, ARGUMENTS ), true, "should be true" );
+assert.equal( doubt( ( ( ) => arguments )( ), ARGUMENTS ), true, "should be true" );
+assert.equal( doubt( [ { "option": { "name": "jlm" } } ], ITERABLE ), true, "should be true" );
 
+assert.equal( doubt( [ { } ], ARRAY_LIKE ), false, "should be false" );
+assert.equal( doubt( Infinity, AS_ARRAY ), false, "should be false" );
 assert.equal( doubt( NaN, AS_ARRAY ), false, "should be false" );
 
 assert.equal( doubt( false, AS_ARRAY ), false, "should be false" );
@@ -18,9 +22,5 @@ assert.equal( doubt( 123, AS_ARRAY ), false, "should be false" );
 assert.equal( doubt( undefined, AS_ARRAY ), false, "should be false" );
 
 assert.equal( doubt( { }, AS_ARRAY ), false, "should be false" );
-
-assert.equal( doubt( [ ], AS_ARRAY ), true, "should be true" );
-
-assert.equal( doubt( ( ( ) => arguments )( ), ARGUMENTS ), true, "should be true" );
 
 console.log( "ok" );
